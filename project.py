7223d38f2f2bf78a15d12658f0359d5f1b9c8c87
px1208 <px12086@gmail.com>
2025-04-07 09:09:08 -0400

    update

--- project.py
+++ project.py
@@ -1,11 +1,24 @@
 import random
 def misty_graden():
-    player_position = 0
+    cornpitcher_pcoosition = 0
     zombie_positon = 20
     PeaShooter_positon =0
+    sunflower_postition=0
     goal_position=30
 
-while player_position <= goal_position or  PeaShooter_positon < goal_position:
+while cornpitcher_position <== goal_position or  PeaShooter_positon < goal_position or sunflower_positon < goal_position:
    print("zombies are coming ")
-   computer_choice = random.choice(["1","2","3"])
-   if 
+   computer_choice = 3
+   zombie_postition= computer_choice
+
+
+def choice():
+    while True:
+        if choice == 1:
+           PeaShooter_positon =1
+
+        if choice ==2:
+            cornpitcher_pcoosition=1
+          
+        if choice ==3:
+            sunflower_postition=1
