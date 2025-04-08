7fbcb0868c333771a0116c406ffca865c3383693
px1208 <px12086@gmail.com>
2025-04-08 09:06:22 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,9 +1,13 @@
 import random
 def misty_graden():
-    cornpitcher_pcoosition = 0
-    zombie_positon = 20
+    cornpitcher_position = 0
+    cornpitcher_hp=10
+    zombie_positon = -20
+    zombie_hp=15
     PeaShooter_positon =0
+    Peashooter=8
     sunflower_postition=0
+    sunflower_hp=20
     goal_position=30
 
 while cornpitcher_position <== goal_position or  PeaShooter_positon < goal_position or sunflower_positon < goal_position:
@@ -14,11 +18,13 @@ while cornpitcher_position <== goal_position or  PeaShooter_positon < goal_posit
 
 def choice():
     while True:
-        if choice == 1:
-           PeaShooter_positon =1
+        if choice == 8:
+           PeaShooter_positon =-1
 
-        if choice ==2:
+        if choice ==9:
             cornpitcher_pcoosition=1
           
-        if choice ==3:
+        if choice ==0:
             sunflower_postition=1
+        if choice == 2:
+        print( Peashoote"
